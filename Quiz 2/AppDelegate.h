4dbfb8e369651cdf867c00fabb13be4e8a16928a
ahernandezg38 <alonso@lemas-Mac.local>
2016-11-07 22:30:45 -0600

    Quiz 2

---
+++ Quiz 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz 2
+//
+//  Created by alonso on 11/5/16.
+//  Copyright © 2016 alonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
